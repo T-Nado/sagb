ed330f82f74d741f0b6c498e8bbfa802187cb81d
Tyrell Whitby <tyrellwhitby@gmail.com>
2020-10-03 20:31:07 -0400

    Animation commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     public AudioSource musicSource;
 
     public AudioClip musicClipOne;
 
     public AudioClip musicClipTwo;
 
-
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -26,22 +25,40 @@ public class NewBehaviourScript : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
         
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) 
+        {
+            musicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L)) 
+        {
+            musicSource.loop = false;
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
         }
     }
 }
